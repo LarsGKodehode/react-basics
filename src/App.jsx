376090d61737dccfebcef24a5950fbf85803eae3
LarsG <lars.ga.solheim@gmail.com>
2023-01-06 12:38:03 +0100

    added array.map component to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // Components
 import ComponentBasic from "./components/ComponentBasic/ComponentBasic"
 import ComponentWithProps from "./components/ComponentWithProps/ComponentWithProps"
+import ComponentArrayMap from "./components/ComponentArrayMap/ComponentArrayMap"
 
 
 // Uncomment the lines with the the components to see them in effect
@@ -12,6 +13,8 @@ function App() {
 
       {/* We are using syntaxt similar to HTML attributes for passing in props (arguments) */}
       {/* <ComponentWithProps title="Title Text" text="Put text you want inside here" /> */}
+
+      {/* <ComponentArrayMap /> */}
     </>
   )
 }
